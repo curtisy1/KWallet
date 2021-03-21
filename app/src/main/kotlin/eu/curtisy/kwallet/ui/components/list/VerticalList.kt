2308107f93d48b95a231501d8db83b78a4677900
curtisy <lausklaus44@gmail.com>
2021-03-21 14:19:27 +0100

    Remove payment logic entirely to focus on card creation

--- app/src/main/kotlin/eu/curtisy/kwallet/ui/components/list/VerticalList.kt
+++ app/src/main/kotlin/eu/curtisy/kwallet/ui/components/list/VerticalList.kt
@@ -2,13 +2,12 @@ package eu.curtisy.kwallet.ui.components.list
 
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import eu.curtisy.kwallet.data.entities.Payment
 import eu.curtisy.kwallet.data.enums.PaymentType
-import eu.curtisy.kwallet.ui.components.PaymentItem
 
 @Composable
 fun <T> VerticalList(
@@ -36,15 +35,15 @@ fun <T> VerticalList(
 @Composable
 @Preview
 private fun VerticalListPreview() {
-    val payments: List<Payment> = listOf(
-        Payment("Education", 200.00, PaymentType.EDUCATION, true),
-        Payment("Credit", 50.00, PaymentType.CREDIT, true),
-        Payment("Food", 20.00, PaymentType.FOOD, false),
-        Payment("Parking", 5.00, PaymentType.PARKING, false),
-        Payment("Generic", 75.51, PaymentType.GENERIC, false)
+    val payments = listOf(
+        "Education",
+        "Credit",
+        "Food",
+        "Parking",
+        "Generic"
     )
 
     VerticalList(items = payments, generatorFunc = {
-        PaymentItem(payment = it, modifier = Modifier.padding(bottom = 24.dp))
+        Text(text = it, modifier = Modifier.padding(bottom = 24.dp))
     })
 }
