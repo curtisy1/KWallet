7a1825067d4c3d791c9ce29ad6e667ce0fb103b4
curtisy <lausklaus44@gmail.com>
2021-04-11 17:15:39 +0200

    Refactor color picker: - Always apply default selection of colors only for now - Remove custom theme, Material should suffice until glass stuff is added

--- app/src/main/kotlin/eu/curtisy/kwallet/ui/components/utils/ColorPicker.kt
+++ app/src/main/kotlin/eu/curtisy/kwallet/ui/components/utils/ColorPicker.kt
@@ -3,32 +3,59 @@ package eu.curtisy.kwallet.ui.components.utils
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.contentColorFor
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Palette
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.luminance
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.flowlayout.FlowRow
 import com.google.accompanist.flowlayout.MainAxisAlignment
 import com.google.accompanist.flowlayout.SizeMode
 
+val ColorPickerColors = listOf(
+    Color(0xFFFFFFFF),
+    Color(0xFFFAFAFA),
+    Color(0x80FF4444),
+    Color(0xFFEF5350),
+    Color(0xFFEC407A),
+    Color(0xFFAB47BC),
+    Color(0xFF7E57C2),
+    Color(0xFF5C6BC0),
+    Color(0xFF42A5F5),
+    Color(0xFF29B6F6),
+    Color(0xFF26C6DA),
+    Color(0xFF26A69A),
+    Color(0xFF66BB6A),
+    Color(0xFF9CCC65),
+    Color(0xFFD4E157),
+    Color(0xFFFFEE58),
+    Color(0xFFFFCA28),
+    Color(0xFFFFA726),
+    Color(0xFFFF7043),
+    Color(0xFF000000),
+)
+
 @Composable
 fun ColorPicker(
-    items: List<Color?>,
-    selectedColor: Color?,
-    onColorSelected: (color: Color?) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    selectedColor: Color,
+    onColorSelected: (color: Color) -> Unit,
+    open: Boolean,
+    onOpen: () -> Unit,
 ) {
     Column(modifier = modifier) {
         FlowRow(
@@ -37,12 +64,15 @@ fun ColorPicker(
             crossAxisSpacing = 4.dp,
             mainAxisSpacing = 4.dp
         ) {
-            items.distinct().forEach { color ->
-                ColorItem(
-                    selected = color == selectedColor,
-                    color = color,
-                    onClick = { onColorSelected(color) }
-                )
+            ColorPickerToggleButton(open, onOpen)
+            if(open) {
+                ColorPickerColors.distinct().forEach { color ->
+                    ColorItem(
+                        selected = color == selectedColor,
+                        color = color,
+                        onClick = { onColorSelected(color) }
+                    )
+                }
             }
         }
     }
@@ -51,12 +81,10 @@ fun ColorPicker(
 @Composable
 fun ColorItem(
     selected: Boolean,
-    color: Color?,
+    color: Color,
     onClick: () -> Unit
 ) {
     val grey400 = Color(0xFFBDBDBD)
-    val blackAlpha20 = Color(0x33000000)
-    val whiteAlpha20 = Color(0x33FFFFFF)
 
     Box(
         modifier = Modifier
@@ -65,53 +93,82 @@ fun ColorItem(
             .size(40.dp)
             .clickable(onClick = onClick)
     ) {
-        if (color != null) {
-            // Transparent background pattern
-            Box(modifier = Modifier.width(20.dp).fillMaxHeight().background(grey400))
-            // Color indicator
-            val colorModifier =
-                if (color.luminance() < 0.1 || color.luminance() > 0.9) {
-                    Modifier
-                        .fillMaxSize()
-                        .background(color)
-                        .border(
-                            width = 1.dp,
-                            color = MaterialTheme.colors.onSurface,
-                            shape = CircleShape
-                        )
-                } else {
-                    Modifier
-                        .fillMaxSize()
-                        .background(color)
-                }
-            Box(
-                modifier = colorModifier
-            ) {
-                if (selected) {
-                    Icon(
-                        Icons.Default.Check,
-                        "",
-                        tint = if (color.luminance() < 0.5) Color.White else Color.Black,
-                        modifier = Modifier.align(Alignment.Center)
-                    )
-                }
-            }
-        } else {
+        // Transparent background pattern
+        Box(
+            modifier = Modifier
+                .width(20.dp)
+                .fillMaxHeight()
+                .background(grey400)
+        )
+        // Color indicator
+        var colorModifier = Modifier
+            .fillMaxSize()
+            .background(color)
+        if (color.luminance() < 0.1 || color.luminance() > 0.9) {
+            colorModifier = colorModifier
+                .border(
+                    width = 1.dp,
+                    color = MaterialTheme.colors.onSurface,
+                    shape = CircleShape
+                )
+        }
+        Box(
+            modifier = colorModifier
+        ) {
             if (selected) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .align(Alignment.Center)
-                        .background(if (isSystemInDarkTheme()) whiteAlpha20 else blackAlpha20)
+                Icon(
+                    Icons.Default.Check,
+                    "",
+                    tint = if (color.luminance() < 0.5) Color.White else Color.Black,
+                    modifier = Modifier.align(Alignment.Center)
                 )
             }
-            // Color null indicator
+        }
+    }
+}
+
+@Composable
+fun ColorPickerToggleButton(
+    open: Boolean,
+    onOpen: () -> Unit,
+) {
+    var buttonModifier = Modifier
+        .padding(4.dp)
+        .clip(CircleShape)
+        .size(40.dp)
+    if (open) {
+        buttonModifier = buttonModifier.background(MaterialTheme.colors.background)
+    }
+    Box(
+        modifier = buttonModifier
+    ) {
+        IconButton(
+            onClick = {
+                onOpen()
+            }
+        ) {
             Icon(
                 Icons.Default.Palette,
                 "",
-                modifier = Modifier.align(Alignment.Center),
                 tint = contentColorFor(MaterialTheme.colors.surface)
             )
         }
     }
+}
+
+@Composable
+@Preview
+fun ColorPickerPreview() {
+    val (colorPickerOpen, onColorPickerOpen) = remember { mutableStateOf(false) }
+    ColorPicker(
+        selectedColor = ColorPickerColors[0],
+        onColorSelected = { },
+        open = colorPickerOpen,
+        onOpen = { onColorPickerOpen(!colorPickerOpen) })
+}
+
+@Composable
+@Preview
+fun ColorItemPreview() {
+    ColorItem(selected = false, color = ColorPickerColors[0], onClick = { })
 }
--- app/src/main/kotlin/eu/curtisy/kwallet/ui/screens/card/CardCreation.kt
+++ app/src/main/kotlin/eu/curtisy/kwallet/ui/screens/card/CardCreation.kt
@@ -9,48 +9,24 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.navigate
 import androidx.navigation.compose.rememberNavController
-import com.google.accompanist.insets.navigationBarsPadding
-import com.google.accompanist.insets.statusBarsPadding
 import com.google.accompanist.insets.systemBarsPadding
 import eu.curtisy.kwallet.data.repositories.PreviewCardRepositoryImpl
 import eu.curtisy.kwallet.ui.components.appbars.TopNavBar
 import eu.curtisy.kwallet.ui.components.creditcard.CardContent
 import eu.curtisy.kwallet.ui.components.utils.ColorPicker
+import eu.curtisy.kwallet.ui.components.utils.ColorPickerColors
 import eu.curtisy.kwallet.ui.navigation.AppRoutes
 import eu.curtisy.kwallet.ui.screens.overview.CardViewModel
 
 @Composable
 fun CardCreation(navController: NavHostController, viewModel: CardViewModel) {
-    val colors = listOf(
-        null,
-        Color(0xFF000000),
-        Color(0xFFFFFFFF),
-        Color(0xFFFAFAFA),
-        Color(0x80FF4444),
-        Color(0xFFEF5350),
-        Color(0xFFEC407A),
-        Color(0xFFAB47BC),
-        Color(0xFF7E57C2),
-        Color(0xFF5C6BC0),
-        Color(0xFF42A5F5),
-        Color(0xFF29B6F6),
-        Color(0xFF26C6DA),
-        Color(0xFF26A69A),
-        Color(0xFF66BB6A),
-        Color(0xFF9CCC65),
-        Color(0xFFD4E157),
-        Color(0xFFFFEE58),
-        Color(0xFFFFCA28),
-        Color(0xFFFFA726),
-        Color(0xFFFF7043)
-    )
-    val (selectedColor, onColorSelected) = remember { mutableStateOf(colors[0]) }
+    val (selectedColor, onColorSelected) = remember { mutableStateOf(ColorPickerColors[0]) }
+    val (colorPickerOpen, onColorPickerOpen) = remember { mutableStateOf(false) }
     val (cardState, saveCardState) = remember { mutableStateOf(viewModel.selectedCard) }
 
     Scaffold(
@@ -75,10 +51,13 @@ fun CardCreation(navController: NavHostController, viewModel: CardViewModel) {
                 Spacer(Modifier.height(20.dp))
                 Column(modifier = Modifier.align(Alignment.Center)) {
                     ColorPicker(
-                        colors,
-                        selectedColor,
-                        onColorSelected,
-                        modifier = Modifier.padding(12.dp)
+                        selectedColor = selectedColor,
+                        onColorSelected = onColorSelected,
+                        modifier = Modifier.padding(12.dp),
+                        open = colorPickerOpen,
+                        onOpen = {
+                            onColorPickerOpen(!colorPickerOpen)
+                        }
                     )
                 }
                 Spacer(Modifier.height(20.dp))
